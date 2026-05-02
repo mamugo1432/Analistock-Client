ae2fc5c63da6811d92b75506825acf0e2b91f5e3
mamugo1432 <mamugo1432@gmail.com>
2026-05-02 19:04:25 +0200

    Añadiendo cambios en la imagen de información del indicador para que no se pueda preseleccionar

--- src/components/MaterialUI/InfoIndicator.tsx
+++ src/components/MaterialUI/InfoIndicator.tsx
@@ -1,17 +1,17 @@
 import { Tooltip } from "@mui/material";
-import infoIcon from "../../assets/info-svgrepo-com.svg"
-export default function InfoIndicator({message}:{message:string}){
-return(     
-<Tooltip
-      placement="bottom-start"
-      arrow
-      title={
-            <p>{message}</p>
-        
-      }
-    >
-        <div className="info-icon">
-            <img src={infoIcon} alt="Icono de información"/>
-            </div>
-    </Tooltip>)
+import infoIcon from "../../assets/info-svgrepo-com.svg";
+export default function InfoIndicator({ message }: { message: string }) {
+  return (
+    <Tooltip placement="bottom-start" arrow title={<p>{message}</p>}>
+      <div className="info-icon">
+        <img
+          src={infoIcon}
+          alt="Icono de información"
+          onContextMenu={(e) => e.preventDefault()}
+          draggable={false}
+          style={{ WebkitTouchCallout: "none", userSelect: "none" }}
+        />
+      </div>
+    </Tooltip>
+  );
 }
